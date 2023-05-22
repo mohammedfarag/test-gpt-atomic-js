e1748ea2d662fad32af953506ecaa7c3261e2aa3
Fego <e.mohammedfarag@gmail.com>
2023-05-22 17:45:44 +0300

    new text

--- components/atoms/Button.js
+++ components/atoms/Button.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Button = ({ text, onClick }) => {
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onClick}>
-      {text}
+      {text}' profile test.
     </button>
   );
 };
